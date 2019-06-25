85aec74839b69dfdcb0fac8e7b91049cf91238bd
정연진 <jduswls@gmail.com>
2019-06-25 23:50:55 +0900

    앱의 전체 헤더 추가

---
+++ src/components/home/header/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./index.scss";
+
+export default function Header(props) {
+  return (
+    <div className="app_license">
+      <div className="app_license_text">MIT license</div>
+    </div>
+  );
+}
