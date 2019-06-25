82bf7769bcb83d1120a2c0089e1347e9f5f11731
정연진 <jduswls@gmail.com>
2019-06-25 23:51:20 +0900

    리액트-라우터 추가

--- src/App.js
+++ src/App.js
@@ -2,11 +2,19 @@ import React from "react";
 import { RiotAPI, MyContext } from "./store";
 import "./App.css";
 import { Main } from "./containers";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Header } from "./components/home";
 
 function App() {
   return (
     <MyContext.Provider value={{ ...RiotAPI() }}>
-      <Main />
+      <Router>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={Main} />
+          <Route exact path="/search/:username" component={Main} />
+        </Switch>
+      </Router>
     </MyContext.Provider>
   );
 }
