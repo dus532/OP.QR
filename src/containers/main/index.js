ead3b90775aba59e5744fb9db9853e6e8c4f41fd
정연진 <jduswls@gmail.com>
2019-06-27 00:14:58 +0900

    메인 화면 수정

--- src/containers/main/index.js
+++ src/containers/main/index.js
@@ -1,19 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import "./index.scss";
 import { AInput, AButton } from "../../components/common";
+import { Redirect } from "react-router-dom";
 
 export default function Main(props) {
+  const [sumname, setSumname] = useState("");
+  const [clickSearch, setClickSearch] = useState(false);
+
+  const handleChange = e => {
+    setSumname(e.target.value);
+  };
+
+  const onSearch = e => {
+    e.preventDefault();
+    setClickSearch(true);
+  };
+
   return (
-    <>
+    <form onSubmit={onSearch}>
       <div className="app-main">
         <div className="app-main-title">OP.QR</div>
         <br />
-        <div className="app-main-sub_title">쉽게 따라하는 전적검색 사이트</div>
+        <div className="app-main-sub_title">
+          쉽게 따라하는 <span>전적검색</span> 사이트
+        </div>
       </div>
-      <div className="app-serach">
-        <AInput placeholder="소환사명을 입력하세요!" />
-        <AButton text="지금 검색하기" />
+      <div className="app-search">
+        <AInput
+          onChange={handleChange}
+          value={sumname}
+          placeholder="소환사명을 입력하세요!"
+        />
+        <AButton type="submit" text="지금 검색하기" />
       </div>
-    </>
+      {clickSearch && <Redirect push to={`/search/${sumname}`} />}
+    </form>
   );
 }
