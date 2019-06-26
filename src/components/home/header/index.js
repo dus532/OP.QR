58288ef5d757fb9a3fb02ca81691b1e0d7266343
정연진 <jduswls@gmail.com>
2019-06-27 00:14:50 +0900

    헤더 부분 좌측 OP.QR 홈 버튼 삽입

--- src/components/home/header/index.js
+++ src/components/home/header/index.js
@@ -1,9 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./index.scss";
 
 export default function Header(props) {
   return (
-    <div className="app_license">
+    <div className="header">
+      <Link to="/">
+        <div className="app_home">OP.QR</div>
+      </Link>
       <div className="app_license_text">MIT license</div>
     </div>
   );
