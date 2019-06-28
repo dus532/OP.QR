3b17ac1384177516ed0ccba4fcea24b31b260330
정연진 <jduswls@gmail.com>
2019-06-29 00:43:18 +0900

    Riot API 주소 추가

--- src/store/index.js
+++ src/store/index.js
@@ -4,12 +4,16 @@ const MyContext = createContext();
 
 const RiotAPI = () => {
   const [URL, setURL] = useState("");
+  const [RiotKey, setRiotKey] = useState("");
 
   useEffect(() => {
-    setURL("URL");
+    setRiotKey("?api_key=RGAPI-343e51ac-0c51-4591-b8c0-de1faf7046e4");
+    setURL(
+      "https://cors-anywhere.herokuapp.com/https://kr.api.riotgames.com/lol/"
+    );
   }, []);
 
-  return { URL };
+  return { URL, RiotKey };
 };
 
 export { MyContext, RiotAPI };
